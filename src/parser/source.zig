c39b690b015d91f73ae194edaeda04c8706a5d90
Onyedika Nwankwo <nwankwobenson29@gmail.com>
2026-08-13 23:14:17 +0100

    feat(lexing-wip): Renamed fields and added switch statments

--- src/parser/source.zig
+++ src/parser/source.zig
@@ -7,54 +7,54 @@ pub const SourceFile = struct {
 };
 
 pub const Span = struct {
-    lo: u32,
-    hi: u32,
+    start: u32,
+    end: u32,
     pub fn to(self: *const Span, span: Span) Span {
-        const high = if (self.hi >= span.hi) self.hi else span.hi;
-        const low = if (self.lo <= span.lo) self.lo else span.lo;
+        const start = if (self.start <= span.start) self.start else span.start;
+        const end = if (self.end >= span.end) self.end else span.end;
         return Span{
-            .hi = high,
-            .lo = low,
+            .start = start,
+            .end = end,
         };
     }
 };
 
 test "span test normal" {
-    const span1 = Span{ .lo = 0, .hi = 1 };
-    const span2 = Span{ .lo = 2, .hi = 3 };
+    const span1 = Span{ .start = 0, .end = 1 };
+    const span2 = Span{ .start = 2, .end = 3 };
     const span3 = span1.to(span2);
-    try std.testing.expectEqual(0, span3.lo);
-    try std.testing.expectEqual(3, span3.hi);
+    try std.testing.expectEqual(0, span3.start);
+    try std.testing.expectEqual(3, span3.end);
 }
 
 test "span test fisrt span encompass second" {
-    const span1 = Span{ .lo = 0, .hi = 4 };
-    const span2 = Span{ .lo = 1, .hi = 2 };
+    const span1 = Span{ .start = 0, .end = 4 };
+    const span2 = Span{ .start = 1, .end = 2 };
     const span3 = span1.to(span2);
-    try std.testing.expectEqual(0, span3.lo);
-    try std.testing.expectEqual(4, span3.hi);
+    try std.testing.expectEqual(0, span3.start);
+    try std.testing.expectEqual(4, span3.end);
 }
 
 test "span test second span encompass first" {
-    const span1 = Span{ .lo = 2, .hi = 4 };
-    const span2 = Span{ .lo = 1, .hi = 5 };
+    const span1 = Span{ .start = 2, .end = 4 };
+    const span2 = Span{ .start = 1, .end = 5 };
     const span3 = span1.to(span2);
-    try std.testing.expectEqual(1, span3.lo);
-    try std.testing.expectEqual(5, span3.hi);
+    try std.testing.expectEqual(1, span3.start);
+    try std.testing.expectEqual(5, span3.end);
 }
 
 test "span test spans disjointed" {
-    const span1 = Span{ .lo = 0, .hi = 3 };
-    const span2 = Span{ .lo = 5, .hi = 7 };
+    const span1 = Span{ .start = 0, .end = 3 };
+    const span2 = Span{ .start = 5, .end = 7 };
     const span3 = span1.to(span2);
-    try std.testing.expectEqual(0, span3.lo);
-    try std.testing.expectEqual(7, span3.hi);
+    try std.testing.expectEqual(0, span3.start);
+    try std.testing.expectEqual(7, span3.end);
 }
 
 test "span test order reversed" {
-    const span1 = Span{ .lo = 5, .hi = 8 };
-    const span2 = Span{ .lo = 1, .hi = 2 };
+    const span1 = Span{ .start = 5, .end = 8 };
+    const span2 = Span{ .start = 1, .end = 2 };
     const span3 = span1.to(span2);
-    try std.testing.expectEqual(1, span3.lo);
-    try std.testing.expectEqual(8, span3.hi);
+    try std.testing.expectEqual(1, span3.start);
+    try std.testing.expectEqual(8, span3.end);
 }
--- src/parser/tokenizer.zig
+++ src/parser/tokenizer.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+
 const source = @import("source.zig");
 const Span = source.Span;
 
@@ -14,6 +15,8 @@ pub const Token = struct {
         .{ "import", .keyword_import },
         .{ "func", .keyword_func },
         .{ "return", .keyword_return },
+        .{ "true", .keyword_true },
+        .{ "false", .keyword_false },
     });
 
     pub fn getKeyword(bytes: []const u8) ?Tag {
@@ -29,6 +32,9 @@ pub const Token = struct {
         keyword_type,
         keyword_func,
         keyword_return,
+        keyword_true,
+        keyword_false,
+        built_in,
         uint8,
         uint16,
         uint32,
@@ -70,6 +76,7 @@ pub const Token = struct {
         string_literal,
         char_literal,
         hex_literal,
+        number_literal,
         binary_literal,
         equal_equal,
         plus_equal,
@@ -93,6 +100,9 @@ pub const Token = struct {
         dot_dot_equal,
         comma,
         underscore,
+        backslash,
+        carat,
+        carat_equal,
 
         pub fn lexeme(self: Tag) ?[]const u8 {
             return switch (self) {
@@ -104,8 +114,6 @@ pub const Token = struct {
                 .multi_line_string_literal,
                 .string_literal,
                 .char_literal,
-                .hex_literal,
-                .binary_literal,
                 => null,
                 .keyword_var => "var",
                 .keyword_const => "const",
@@ -169,7 +177,212 @@ pub const Token = struct {
                 .dot_dot => "..",
                 .dot_dot_equal => "..=",
                 .comma => ",",
+                .backslash => "\\",
+                .underscore => "_",
+                .carat => "^",
+                .carat_equal => "^=",
+            };
+        }
+        pub fn symbol(tag: Tag) []const u8 {
+            return tag.lexeme() orelse switch (tag) {
+                .invalid => "invalid Token",
+                .eof => "end of file",
+                .identifier => "an identifier",
+                .int_literal => "an int literal",
+                .float_literal => "a float literal",
+                .multi_line_string_literal => "a multi line string literal",
+                .string_literal => "a string literal",
+                .char_literal => "a char literal",
+                else => unreachable,
             };
         }
     };
+};
+pub const Tokenizer = struct {
+    buffer: [:0]const u8,
+    index: u32,
+
+    pub fn dump(self: *Tokenizer, token: *const Token) void {
+        std.debug.print("{s}\"{s}\"\n", .{
+            @tagName(token.tag),
+            self.buffer[token.span.lo..token.span.hi],
+        });
+    }
+
+    pub fn init(buffer: [:0]const u8) Tokenizer {
+        return .{
+            .buffer = buffer,
+            .index = if (std.mem.startsWith(u8, buffer, "\xEF\xBB\xBF")) 3 else 0,
+        };
+    }
+
+    const State = enum {
+        start,
+        invalid,
+        expect_newline,
+        string_literal,
+        identifier,
+        saw_at_sign,
+        builtin,
+        equal,
+        bang,
+        pipe,
+        l_angle,
+        l_angle_l_angle,
+        r_angle,
+        r_angle_r_angle,
+        slash,
+        backslash,
+        carat,
+        plus,
+        minus,
+        asterisk,
+        amperstand,
+        dot,
+        dot_dot,
+        number_literal,
+        char_literal,
+        char_literal_backslash,
+        int,
+        float,
+    };
+
+    pub fn next(self: *Tokenizer) Token {
+        var result: Token = .{
+            .tag = undefined,
+            .span = .{
+                .start = self.index,
+                .end = undefined,
+            },
+        };
+
+        state: switch (State.start) {
+            .start => switch (self.buffer[self.index]) {
+                0 => {
+                    if (self.index == self.buffer.len) {
+                        return .{
+                            .tag = .eof,
+                            .span = .{
+                                .start = self.index,
+                                .end = self.index,
+                            },
+                        };
+                    }
+                },
+                ' ', '\n', '\t', '\r' => {
+                    self.index += 1;
+                    result.span.start = self.index;
+                    continue :state .start;
+                },
+                '"' => {
+                    result.tag = .string_literal;
+                    continue :state .string_literal;
+                },
+                '\'' => {
+                    result.tag = .char_literal;
+                    continue :state .char_literal;
+                },
+
+                'a'...'z', 'A'...'Z', '_' => {
+                    result.tag = .identifier;
+                    continue :state .identifier;
+                },
+                '@' => continue :state .saw_at_sign,
+                '&' => continue :state .amperstand,
+                '=' => continue :state .equal,
+                '!' => continue :state .pipe,
+                '<' => continue :state .l_angle,
+                '>' => continue :state .r_angle,
+                '!' => continue :state .bang,
+                '^' => continue :state .carat,
+                '\\' => continue :state .backslash,
+                '+' => continue :state .plus,
+                '-' => continue :state .minus,
+                '.' => continue :state .dot,
+                ';' => {
+                    result.tag = .semicolon;
+                    self.index += 1;
+                },
+                ',' => {
+                    result.tag = .comma;
+                    self.index += 1;
+                },
+                ':' => {
+                    result.tag = .colon;
+                    self.index += 1;
+                },
+                '(' => {
+                    result.tag = .l_paren;
+                    self.index += 1;
+                },
+                ')' => {
+                    result.tag = .r_paren;
+                    self.index += 1;
+                },
+                '{' => {
+                    result.tag = .l_brace;
+                    self.index += 1;
+                },
+                '}' => {
+                    result.tag = .r_paren;
+                    self.index += 1;
+                },
+                '[' => {
+                    result.tag = .l_bracket;
+                    self.index += 1;
+                },
+                ']' => {
+                    result.tag = .l_bracket;
+                    self.index += 1;
+                },
+                '0'...'9' => {
+                    result.tag = .number_literal;
+                    self.index += 1;
+                    continue :state .int;
+                },
+                else => continue :state .invalid,
+            },
+            .expect_newline => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    0 => {
+                        if (self.index == self.buffer.len) {
+                            result.tag = .invalid;
+                        } else {
+                            continue :state .invalid;
+                        }
+                    },
+                    '\n' => {
+                        self.index += 1;
+                        result.span.start = self.index;
+                        continue :state .start;
+                    },
+                    else => continue :state .invalid,
+                }
+            },
+
+            .invalid => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    0 => if (self.index == self.buffer.len) {
+                        result.tag = .invalid;
+                    } else {
+                        continue :state .invalid;
+                    },
+                    '\n' => result.tag = .invalid,
+                    else => continue :state .invalid,
+                }
+            },
+            .saw_at_sign => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    0, '\n' => result.tag = .invalid,
+                    'a'...'z', 'A'...'Z', '_' => {
+                        result.tag = .built_in;
+                    },
+                    else => continue :state .invalid,
+                }
+            },
+        }
+    }
 };
